5c470857b50cb0e77f1ef3102d3ad242361de1de
Windstep <icq431@yandex.ru>
2021-02-15 07:02:54 +0300

    Updated to phpunit 9

--- tests/DataSet/CompositeDataSetTest.php
+++ tests/DataSet/CompositeDataSetTest.php
@@ -141,12 +141,10 @@ asdflkjsadf asdfsadfhl "adsf, halsdf" sadfhlasdf'
         $this->assertEquals(4, $compositeDataSet->getTable('table3')->getRowCount());
     }
 
-    /**
-     * @expectedException           InvalidArgumentException
-     * @expectedExceptionMessage    There is already a table named table3 with different table definition
-     */
     public function testExceptionOnIncompatibleTablesSameTableNames(): void
     {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('There is already a table named table3 with different table definition');
         $inCompatibleTableMetaData = new DefaultTableMetadata(
             'table3',
             ['table3_id', 'column13', 'column14', 'column15', 'column16']
@@ -166,12 +164,10 @@ asdflkjsadf asdfsadfhl "adsf, halsdf" sadfhlasdf'
         ]);
     }
 
-    /**
-     * @expectedException           InvalidArgumentException
-     * @expectedExceptionMessage    There is already a table named table3 with different table definition
-     */
     public function testExceptionOnIncompatibleTablesSameTableNames2(): void
     {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('There is already a table named table3 with different table definition');
         $inCompatibleTableMetaData = new DefaultTableMetadata(
             'table3',
             ['table3_id', 'column13', 'column14', 'column15', 'column16']
